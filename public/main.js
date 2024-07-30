7df15ec9276bf6a3dfa9b0ec05ef11fa19c3cbd9
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-07-30 21:49:47 +0200

    Binding rust to js

--- public/main.js
+++ public/main.js
@@ -1,11 +1,21 @@
-function init(){
+async function init(){
+  let rustApp = null;
+  try {
+    rustApp = await import('../pkg');
+  } catch (error) {
+    console.log(error);
+    return;
+    
+  }
+  console.log(rustApp);
+
   const input = document.getElementById('upload');
   const fileReader = new FileReader();
 
-  fileReader.onload = () =>{
+  fileReader.onloadend = () =>{
     const base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/g,'')
-    console.log(input.files[0]);
-    console.log(base64);
+    
+    rustApp.grayscale(base64);
   }
 
 
