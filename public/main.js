5b2161ba0618e1e106b3abb0590e45a870b2bea4
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-07-31 00:58:30 +0200

    Encoding an image

--- public/main.js
+++ public/main.js
@@ -1,24 +1,25 @@
-async function init(){
+async function init() {
   let rustApp = null;
   try {
     rustApp = await import('../pkg');
   } catch (error) {
     console.log("Failed to import package");
     console.log(error);
-    return; 
+    return;
   }
   console.log(rustApp);
 
   const input = document.getElementById('upload');
   const fileReader = new FileReader();
 
-  fileReader.onloadend = () =>{
-    const base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/g,'')
-    rustApp.grayscale(base64);
+  fileReader.onloadend = () => {
+    const base64 = fileReader.result.replace(/^data:image\/(png|jpeg|jpg);base64,/g, '')
+    let image_data_url = rustApp.grayscale(base64);
+    document.getElementById('new-img').setAttribute('src',image_data_url);
   }
 
 
-  input.addEventListener('change', ()=>{
+  input.addEventListener('change', () => {
     fileReader.readAsDataURL(input.files[0]);
   })
 }
